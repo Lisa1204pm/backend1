f02a84a8861d8efedae3df3ef61efd60c7420830
Lisa1204pm <119533643+Lisa1204pm@users.noreply.github.com>
2023-01-18 15:41:19 -0800

    Update database.js

--- database.js
+++ database.js
@@ -6,8 +6,7 @@ const pool = mysql
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQL_PORT || 3305,
+    database: process.env.MYSQL_DATABASE
   })
   .promise()
 async function getImages() {
