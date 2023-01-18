3bcc01b4c5f7d9024c3680bb25f5c29172ee60b0
lisa1204pm <dlee560@my.bcit.ca>
2023-01-18 15:19:20 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express = require('express')
+const fs = require('fs')
+const multer = require('multer')
+const database = require('./database')
+const upload = multer({ dest: 'images/' })
+const app = express()
+app.use(express.static('dist'))
+
+app.get("/api/images", async (req, res) => {
+    const getImages = await database.getImages()
+    res.send(getImages)
+})
+
+app.get('/images/:imageName', (req, res) => {
+    const imageName = req.params.imageName
+    const readStream = fs.createReadStream(`images/${imageName}`)
+    readStream.pipe(res)
+})
+
+app.post('/api/images', upload.single('image'), async (req, res) => {
+    const imagePath = 'images/' + req.file.filename
+    const description = req.body.description
+    const createImages = await database.addImage(imagePath, description)
+    console.log(description, imagePath)
+    res.send(createImages)
+})
+
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log("Server is running on port " + port))
